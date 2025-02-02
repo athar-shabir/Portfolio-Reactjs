8465beb3494d9c89aeae82a13186fbe19fb428ae
athar-shabir <your_email@example.com>
2025-02-02 13:31:04 +0530

    Responsive 2 components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/Contact/Contact";
+import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
@@ -16,10 +16,10 @@ const App = () => {
       <Hero/>
       </section>
     <section id="Services"><Parallax type="services"/></section>
-    <section><Services/></section>
-    <section id="Portfolio"><Parallax type="portfolio"/></section>
-    <Portfolio/>
-    <section id="Contact"><Contact /></section>
+    {/* <section><Services/></section> */}
+    {/* <section id="Portfolio"><Parallax type="portfolio"/></section> */}
+    {/* <Portfolio/> */}
+    {/* <section id="Contact"><Contact /></section> */}
   </div>
   );
 };
