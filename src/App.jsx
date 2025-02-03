1c9d83b5a84b9561bb0a71134ab995f09f6d5280
athar-shabir <your_email@example.com>
2025-02-03 15:33:56 +0530

    Responsive components Pending

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ const App = () => {
       <Hero/>
       </section>
     <section id="Services"><Parallax type="services"/></section>
-    {/* <section><Services/></section> */}
-    {/* <section id="Portfolio"><Parallax type="portfolio"/></section> */}
+    <section><Services/></section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     {/* <Portfolio/> */}
     {/* <section id="Contact"><Contact /></section> */}
   </div>
