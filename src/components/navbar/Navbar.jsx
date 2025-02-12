12e614feea97bb58c1d39926f0bef4227a36c3d9
athar-shabir <your_email@example.com>
2025-02-12 19:51:51 +0530

    added some tag on the navbar of social media

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = () => {
         <div className="wrapper">
             <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Athar Shabir</motion.span>
             <div className="Social">
-                <a href="#"><img src="facebook.png" alt="" /></a>
-                <a href="#"><img src="/instagram.png" alt="" /></a>
-                <a href="#"><img src="/youtube.png" alt="" /></a>
-                <a href="#"><img src="dribbble.png" alt="" /></a>
+                <a href="https://www.facebook.com/athar.shabir/" target="_blank"><img src="facebook.png" alt="" /></a>
+                <a href="https://www.instagram.com/athar.shabir/" target="_blank"><img src="/instagram.png" alt="" /></a>
+                <a href="https://github.com/athar-shabir" target="_blank"><img src="github.png" alt="" /></a>
+                <a href="https://www.linkedin.com/in/athar-shabir-28ab54215/" target="_blank"><img src="linkedin.png" alt="" /></a>
             </div>
         </div>
     </div>
